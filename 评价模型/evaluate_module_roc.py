4141c7a2640679aec4c51a044e459b8c4bd2d999
liangchengbo <466312597@qq.com>
2018-11-30 00:39:55 +0100

    Update evaluate_module_roc.py

--- 评价模型/evaluate_module_roc.py
+++ 评价模型/evaluate_module_roc.py
@@ -10,14 +10,16 @@ from sklearn.metrics import roc_auc_score
 
 label=	[1,0,1,0,1,0,1,0,1,1]#10个ground_true ,1为有用positive，0为垃圾,
 pre=	[1,0,1,0,1,0,0,0,0,0]#预测
-proba=	[0.9,0.9,0.9,0.2,0.9,0.2,0.9,0.2,0.9,0.9]#这样效果就最好了，预测1的时候概率很高，预测0的时候概率就很低
-												#这个概率应该说是为1的概率，就是属于正类的概率，roc曲线整个过程针对的正类只有一个
+positive_proba=	[0.9,0.9,0.9,0.2,0.9,0.2,0.9,0.2,0.9,0.9]
+#
+#
 
-dict={'label':label,'pre':pre}
+dict={'label':label,'pre':pre,'pro':positive_proba}
 df1=pd.DataFrame(dict)
 print(df1)
 
 fpr,tpr,thresholds=metrics.roc_curve(df1['label'],df1['pre'],pos_label=1)
+#第一个参数是guangtrue分类，第二是预测类，或者是positive的softmax概率(不是N的),三表明1才是positive
 
 plt.plot(fpr,tpr)
 plt.show()
