f24b06e1e0fb53b1fc24829cce703f56fa962087
liangchengbo <466312597@qq.com>
2018-12-01 20:00:55 +0100

    Create python_document_process.py

---
+++ path&str/python_document_process.py
@@ -0,0 +1,16 @@
+#
+import os
+import shutil
+
+a=os.listdir(path)
+print(a)
+for i in a:#遍历每个元素
+	b=i.split(",")
+	print(b)
+	
+#移动文件
+# shutil.move(object,target) #将object移动到target里面，都为全名
+#重命名
+# os.rename(target,'C:\lcb\learning_python_git\extradoc\TP-MLP\which_symbol\serve_version\b\abcde')
+#复制文件
+# shutil.copytree("olddir","newdir") #olddir和newdir都只能是目录，且newdir必须不存在
