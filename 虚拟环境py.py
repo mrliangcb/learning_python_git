145bf0725db34c6b6adbaa0f84761b0740968b2b
liangchengbo <466312597@qq.com>
2018-12-19 22:03:48 +0100

    nan

---
+++ 虚拟环境py.py
@@ -0,0 +1,29 @@
+#独立的python虚拟环境
+Calibration
+dir
+pip install virtualenv
+virtualenv homography
+进入文件夹Calibration
+Calibration>homography\Scripts\activate  进入这里active
+pip install python-opencv numpy 安装虚拟环境的opencv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
