06aa1cfb24a3979768bfbb34d900de75c771c8c7
liangchengbo <466312597@qq.com>
2019-03-13 09:58:06 +0100

    a

--- path&str/str_path.py
+++ path&str/str_path.py
@@ -5,32 +5,9 @@ import random
 import glob
 import os
 
-#一、#format用法
-print('hello {:0>3}'.format(1))#:.2f 保留小数点后两位
-#:.2%百分比格式
-#{:0>3}左边补0，直到整个数长度为3   <为右边补
-#%.2f保留2位小数
-
-# i= "%06d" %3#也一样
 
 
-path=r'abc{0:05d}'.format(123)#这个写法比较好
-print(path)
 
-#二、join用法
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-#三、找出文件名glob用法
-#path=r'C:\Users\mrliangcb\Desktop\note\extra_feature\nparray'
-#tmp=glob.glob(r'{}\*.inkml'.format(path)) #找出符合条件的全名
-#如果有多级目录还可以用*代替 例如a\*\*\*.inkml 中间*会自动遍历
-
-
-#image_list = glob.glob(equal_path + r'*.png')
-#for im_name in image_list:
-
-#四、listdir用法
-#tmp2=os.listdir(path)#获得当前文件夹下所有文件名
 
 #五replace用法
 str=r'abcdef'
