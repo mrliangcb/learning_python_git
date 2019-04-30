6824a71dd548b0eed42a9a2763127c3ab735a305
chengboliang <chengbo.liang@etu.univ-nantes.fr>
2019-05-01 05:46:44 +0800

    Update 堆栈队列.py

--- 算法/堆栈队列.py
+++ 算法/堆栈队列.py
@@ -15,7 +15,7 @@
 			# stack.append(c) #压入用append
 		# elif not stack or paren_map[c]!=stack.pop() #如果是右边元，但是又不和栈顶匹配，就返回不匹配
 			# return Fales
-		# return not stack
+		# return not stack #如果遍历完了，检查一下是否空，空则返回true
 
 a={'a':'b'} #检查b在Key中有出现吗，不是value
 print('b' in a) #False
@@ -23,6 +23,8 @@ b=[1,2,3]
 print(b.pop()) #pop(0)打出栈底
 print(b)
 
+
+#replace"()",""   从小到大匹配然后消除,时间复杂度不好计算
 
 
 
