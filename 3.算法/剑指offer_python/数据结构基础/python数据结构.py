64bc56bb3e0161ba32250c3a19a72cfbd5565b05
chengboliang <chengbo.liang@etu.univ-nantes.fr>
2019-08-01 18:36:10 +0800

    algorithm

--- 3.算法/剑指offer_python/数据结构基础/python数据结构.py
+++ 3.算法/剑指offer_python/数据结构基础/python数据结构.py
@@ -6,11 +6,6 @@
 # https://blog.csdn.net/qq_28304687/article/details/79088491 
 #https://blog.csdn.net/liangguohuan/article/details/7088265
 
-#树节点
-class Node:
-	def __init__(self,val):
-		self.val = val
-		self.left,self.right=None,None
 
 
 # (1)list
@@ -28,7 +23,7 @@ print(list_a)
 
 
 # (2)dict
-# dict是哈希表，给出key就能马上查到o1,search搜索o1
+# dict是哈希表，根据Key查找value，时间复杂度o1
 
 # 搜索
 a={'a':'b'} #检查b在Key中有出现吗，不是value
@@ -38,7 +33,7 @@ print('b' in a) #False
 
 # (3) set
 #set 的查找和add也是o1的，哈希结构,搜索是与list最大的不同
-
+# 具体看(数据结构基础)中(set应用)
 
 # (4) deque
 # deque 双向队列，搜索为ON 
@@ -52,7 +47,11 @@ sorted(list)
 2^x=len
 x=log2(n)
 
-
+# (5)树节点
+class Node:
+	def __init__(self,val):
+		self.val = val
+		self.left,self.right=None,None
 
 
 
---
+++ 3.算法/剑指offer_python/数据结构基础/set应用.py
@@ -0,0 +1,63 @@
+
+a=set([1,2,3,5])
+b=set([3,4,5])
+print(a)
+
+
+#求交并差集
+print("\n\n ####### 交集并集差集 ########")
+print('a:',a)
+print('b',b)
+print("求交集intersection:",a&b)
+print("求并集union:",a|b)
+print("求差集union:",b-a) #可以小-大
+
+if 4 in a: print("4 in a")
+if 3 in a: print("3 in a")
+
+
+
+#增加元素
+# a.add()
+
+#删除
+print("\n\n #######删除元素########")
+c=a
+print('原c:',c)
+c.remove(3)
+print('c删除3:',c)
+# c.remove(100)#出错
+c.discard(100) #即使set中没有100也不报错
+
+# 删除所有元素 a.clear()
+
+
+
+
+#长度
+print("\n\n ####### 长度 ########")
+print("长度:",len(a))
+
+#判断子集
+print("\n\n #######判断子集########")
+print('a:',a,'b:',b)
+print('b是a的子集吗:',b<=a)
+print('[5]是a的子集吗:',set([5]).issubset(a))  #前是后的子集吗
+
+
+#出集
+print("\n\n ####### 出集 ########")
+print('原a:',a)
+print(a.pop(),a.pop(),a)#pop出左边的元素，为空则出错
+
+
+
+
+
+
+
+
+
+
+
+
